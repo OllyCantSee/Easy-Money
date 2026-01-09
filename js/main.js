e7330323975070d809e035fc350817d60e740ed9
BuildTools <unconfigured@null.spigotmc.org>
2026-01-09 17:04:04 +0000

    Add fun dynamic messages when entering monthly income

--- js/main.js
+++ js/main.js
@@ -2,21 +2,15 @@
 // Constants & DOM Elements
 // -------------------
 
-const PERCENTAGES = {
-    stocks: 0.6,
-    cash: 0.3,
-    spending: 0.1
-};
-
+const PERCENTAGES = {stocks: 0.6, cash: 0.3, spending: 0.1};
 const MIN_INCOME = 0.01
-const form = document.getElementById("income-form");
-const incomeInput = document.getElementById("income");
 
+const incomeInput = document.getElementById("income");
 const stocksOutput = document.getElementById("stocks-isa");
 const cashOutput = document.getElementById("cash-isa");
 const spendingOutput = document.getElementById("spending");
-
 const errorMessage = document.getElementById("error-message");
+const funText = document.getElementById("fun-text")
 
 //-------------------
 // Functions
@@ -36,19 +30,34 @@ function updateResults(results) {
     spendingOutput.textContent = `£${results.spending.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
 }
 
+function funMessage(incomeValue) {
+    if (incomeValue <= 10) {
+        funText.textContent = "not really..."
+    } else if (incomeValue <= 100) {
+        funText.textContent = "yeah not bad..."
+    } else if (incomeValue <= 1000) {
+        funText.textContent = "yeah its good!"
+    } else if (incomeValue <= 2000) {
+        funText.textContent = "WOO, yes it was!"
+    } else {
+        funText.textContent = "GOD YES!"
+    }
+}
+
 //-------------------
 // Event Listeners
 // -------------------
 
-incomeInput.addEventListener('input', function(event){
+incomeInput.addEventListener('input', () => {
     const incomeValue = parseFloat(incomeInput.value.trim());
 
     if(isNaN(incomeValue) || incomeValue < MIN_INCOME) {
-        errorMessage.textContent = "Please add an income greater than 0";
+        errorMessage.textContent = "Gotta be more than 0!";
         return;
     }
-    
+
     errorMessage.textContent = "";
+    funMessage(incomeValue)
 
     const results = incomeCalculations(incomeValue);
     updateResults(results);
